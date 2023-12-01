713bc9d5c7be5e5a0b418b6aa5e0209fc78526c5
afoix <afoix@ebi.ac.uk>
2023-12-01 05:52:57 +0000

    prepare for tomorrow

---
+++ day2/day2_part1.py
@@ -0,0 +1,23 @@
+
+document = open('day1/day1_input.txt', 'r')
+lines = document.readlines()
+total = 0
+
+for line in lines:
+    leftnum = None
+    for c in line:
+        if c in "0123456789":
+            leftnum = int(c)
+            break
+    
+    rightnum = None
+    for i in range(len(line)-1, -1, -1):
+        if line[i] in "0123456789":
+            rightnum = int(line[i])
+            break
+
+    num = leftnum * 10 + rightnum
+    total += num
+
+print(total)
+

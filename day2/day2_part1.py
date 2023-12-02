aac43dd05abf5c9ef71c49e506608dc1a5b67e5f
afoix <afoix@ebi.ac.uk>
2023-12-02 06:01:20 +0000

    day 2

--- day2/day2_part1.py
+++ day2/day2_part1.py
@@ -1,23 +1,46 @@
-
-document = open('day1/day1_input.txt', 'r')
+import re
+# contained only 12 red cubes, 13 green cubes, and 14 blue cubes
+document = open('day2/day2_input.txt', 'r')
 lines = document.readlines()
-total = 0
 
-for line in lines:
-    leftnum = None
-    for c in line:
-        if c in "0123456789":
-            leftnum = int(c)
-            break
+id_count = []
+
+# Example line: Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+# Game 99: 3 green, 2 blue, 1 red; 15 red, 8 blue, 7 green; 18 red, 12 blue, 2 green
+def parse_line(line):
+    # Regex expression to parse the game and its ID
+    parse_game = re.compile('Game\s+(\d+):(.*)')
+    game_id, rest = parse_game.search(line).groups()
+    game_id = int(game_id)
+    # parse the rest to extract the blocks
+    rest = [x.split(",") for x in rest.split(';')]
+
+    # flatten the list, convert the two list in a single one
+    rest = [item for sublist in rest for item in sublist]
     
-    rightnum = None
-    for i in range(len(line)-1, -1, -1):
-        if line[i] in "0123456789":
-            rightnum = int(line[i])
-            break
+    # parse the blocks
+    parse_block = re.compile('\s*(\d+)\s+(\w+)')
+    dict_blocks = {"red": [], "green": [], "blue": []}
+    for x in rest:
+        v, k = parse_block.search(x).groups()
+        dict_blocks[k].append(int(v))
+    print(dict_blocks)
+    
+    max_dict = {}
+    for k, v in dict_blocks.items():
+        max_dict[k] = max(v)
+    
+    # 12 red cubes, 13 green cubes, and 14 blue cubes?
+    if max_dict["red"] > 12 or max_dict["green"] > 13 or max_dict["blue"] > 14:
+        return None
+    else:
+        return game_id
+    
 
-    num = leftnum * 10 + rightnum
-    total += num
+id_count = 0
+for line in lines:
+    game_id = parse_line(line)
+    if game_id:
+        id_count += game_id
 
-print(total)
-
+print(id_count)

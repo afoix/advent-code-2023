d654ec7f6fa9a73fa3619ccc6fa26a0f1d2b0c16
afoix <afoix@ebi.ac.uk>
2023-12-03 06:54:19 +0000

    day 3 done

--- day3/day3_part1.py
+++ day3/day3_part1.py
@@ -1,46 +1,49 @@
 import re
-# contained only 12 red cubes, 13 green cubes, and 14 blue cubes
-document = open('day2/day2_input.txt', 'r')
+
+document = open('day3/day3_input.txt', 'r')
+
 lines = document.readlines()
 
-id_count = []
-
-# Example line: Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-# Game 99: 3 green, 2 blue, 1 red; 15 red, 8 blue, 7 green; 18 red, 12 blue, 2 green
-def parse_line(line):
-    # Regex expression to parse the game and its ID
-    parse_game = re.compile('Game\s+(\d+):(.*)')
-    game_id, rest = parse_game.search(line).groups()
-    game_id = int(game_id)
-    # parse the rest to extract the blocks
-    rest = [x.split(",") for x in rest.split(';')]
-
-    # flatten the list, convert the two list in a single one
-    rest = [item for sublist in rest for item in sublist]
-    
-    # parse the blocks
-    parse_block = re.compile('\s*(\d+)\s+(\w+)')
-    dict_blocks = {"red": [], "green": [], "blue": []}
-    for x in rest:
-        v, k = parse_block.search(x).groups()
-        dict_blocks[k].append(int(v))
-    print(dict_blocks)
-    
-    max_dict = {}
-    for k, v in dict_blocks.items():
-        max_dict[k] = max(v)
-    
-    # 12 red cubes, 13 green cubes, and 14 blue cubes?
-    if max_dict["red"] > 12 or max_dict["green"] > 13 or max_dict["blue"] > 14:
-        return None
-    else:
-        return game_id
-    
-
-id_count = 0
+numbers = []
+symbols = []
 for line in lines:
-    game_id = parse_line(line)
-    if game_id:
-        id_count += game_id
+    # Find an ocurrance of a number in the line
+    numbers.append([m for m in re.finditer('\d+', line)])
+    symbols.append([m for m in re.finditer('[^.\d\n]', line)])
 
-print(id_count)
+engine_num = []
+for i, nums in enumerate(numbers):
+    for num in nums:
+        done = False
+        if not done and i > 0:
+            print("entering above")
+            syms = symbols[i-1]
+            for sym in syms:
+                if (sym.start() >= num.start() - 1) and (sym.start() <= num.end()):
+                    engine_num.append(int(num.group()))
+                    done = True
+                    print("adjacent from above: {num} and {sym}".format(num=num, sym=sym))
+                    break
+
+        if not done and i < len(numbers) - 1:
+            print("entering below")
+            syms = symbols[i+1]
+            for sym in syms:
+                if (sym.start() >= (num.start() - 1)) and (sym.start() <= num.end()):
+                    engine_num.append(int(num.group()))
+                    done = True
+                    print("adjacent from below: {num} and {sym}".format(num=num, sym=sym))
+                    break
+
+        if not done:
+            print("entering side")
+            syms = symbols[i]
+            for sym in syms:
+               if (sym.start() == num.start() - 1) or (sym.start() == num.end()):
+                    engine_num.append(int(num.group()))
+                    print("adjacent from side: {num} and {sym}".format(num=num, sym=sym))
+                    break
+        
+
+print(sum(engine_num))
+
--- day3/day3_part2.py
+++ day3/day3_part2.py
@@ -1,45 +1,46 @@
-
-    
-
-
 import re
-# contained only 12 red cubes, 13 green cubes, and 14 blue cubes
-document = open('day2/day2_input.txt', 'r')
+
+document = open('day3/day3_input.txt', 'r')
+#document = open('day3/alex_input.txt', 'r')
+
 lines = document.readlines()
 
-id_count = []
-
-# Example line: Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-# Game 99: 3 green, 2 blue, 1 red; 15 red, 8 blue, 7 green; 18 red, 12 blue, 2 green
-def parse_line(line):
-    # Regex expression to parse the game and its ID
-    parse_game = re.compile('Game\s+(\d+):(.*)')
-    game_id, rest = parse_game.search(line).groups()
-    game_id = int(game_id)
-    # parse the rest to extract the blocks
-    rest = [x.split(",") for x in rest.split(';')]
-
-    # flatten the list, convert the two list in a single one
-    rest = [item for sublist in rest for item in sublist]
-    
-    # parse the blocks
-    parse_block = re.compile('\s*(\d+)\s+(\w+)')
-    dict_blocks = {"red": [], "green": [], "blue": []}
-    for x in rest:
-        v, k = parse_block.search(x).groups()
-        dict_blocks[k].append(int(v))
-    print(dict_blocks)
-    
-    max_dict = {}
-    for k, v in dict_blocks.items():
-        max_dict[k] = max(v)
-    
-    return max_dict["red"] *  max_dict["green"] * max_dict["blue"]
-    
-
-power_sets = 0
+numbers = []
+symbols = []
 for line in lines:
-    # sum of the power of these sets
-    power_sets += parse_line(line)
+    # Find an ocurrance of a number in the line
+    numbers.append([m for m in re.finditer('\d+', line)])
+    symbols.append([m for m in re.finditer('\*', line)])
+print(symbols)
 
-print(power_sets)
+result = 0
+for i, syms in enumerate(symbols):
+    for sym in syms:
+        engine_num = []
+        if i > 0:
+            nums = numbers[i-1]
+            for num in nums:
+                if (sym.start() >= num.start() - 1) and (sym.start() <= num.end()):
+                    engine_num.append(int(num.group()))
+                    
+
+        if i < len(numbers) - 1:
+            print("entering below")
+            nums = numbers[i+1]
+            for num in nums:
+                if (sym.start() >= (num.start() - 1)) and (sym.start() <= num.end()):
+                    engine_num.append(int(num.group()))
+
+
+        nums = numbers[i]
+        for num in nums:
+            if (sym.start() == num.start() - 1) or (sym.start() == num.end()):
+                engine_num.append(int(num.group()))
+        
+        if len(engine_num) == 2:
+            result += engine_num[0] * engine_num[1]
+        
+
+print(result)
+
+    

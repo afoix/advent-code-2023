8b392501be5d425d1534f4b0ec9d9940fb26cab7
afoix <afoix@ebi.ac.uk>
2023-12-07 15:33:40 +0000

    7th day of advent of code

---
+++ day7/day7_part2.py
@@ -0,0 +1,121 @@
+from functools import cmp_to_key
+
+def cardScore(card):
+  return {
+    'A': 12
+  , 'K': 11
+  , 'Q': 10
+  , 'T': 9
+  , '9': 8
+  , '8': 7
+  , '7': 6
+  , '6': 5
+  , '5': 4
+  , '4': 3
+  , '3': 2
+  , '2': 1
+  , 'J': 0
+  }.get(card, 0)
+
+def handScore(handtype):
+  return {
+    'Five of a kind': 6
+  , 'Four of a kind': 5
+  , 'Full house': 4
+  , 'Three of a kind': 3
+  , 'Two pair': 2
+  , 'One pair': 1
+  , 'High card': 0
+  }.get(handtype, 0)
+
+def cmpHands(a, b):
+  cmpPairs = list(zip(a, b))
+  if (cardScore(cmpPairs[0][0]) > cardScore(cmpPairs[0][1])):
+    return 1
+  elif (cardScore(cmpPairs[0][1]) > cardScore(cmpPairs[0][0])):
+    return -1
+  elif len(cmpPairs[1:]) > 0:
+    return cmpHands(*zip(*cmpPairs[1:]))
+  else:
+    return 0
+
+def classifyHand(hand):
+  cardcount = {}
+  for c in hand:
+    try:
+      cardcount[c] += 1
+    except:
+      cardcount[c] = 1
+
+  # handle jokers
+  cardtypes = len(cardcount)
+  jokersub = None
+  try:
+    if cardtypes == 1:
+      cardcount['A'] = cardcount.pop('J')
+      jokersub = 'A'
+    else:
+      n = cardcount.pop('J')
+      x = list(cardcount.items())
+      x.sort(key=lambda x: cardScore(x[0]))
+      x.sort(key=lambda x: x[1])
+      x[-1] = (x[-1][0], x[-1][1] + n)
+      jokersub = x[-1][0]
+      cardcount = dict(x)
+  finally:
+    cardtypes = len(cardcount)
+    handtype = None
+    if cardtypes == 1:
+      handtype = 'Five of a kind'
+    elif cardtypes == 2 and 1 in cardcount.values():
+      handtype = 'Four of a kind'
+    elif cardtypes == 2:
+      handtype = 'Full house'
+    elif cardtypes == 3 and 3 in cardcount.values():
+      handtype = 'Three of a kind'
+    elif cardtypes == 3:
+      handtype = 'Two pair'
+    elif cardtypes == 4:
+      handtype = 'One pair'
+    else:
+      handtype = 'High card'
+    return (handtype, cardcount)
+
+def parse_input(filename):
+  file = open(filename, 'r')
+  hands = []
+  for line in file.readlines():
+    es = line.split()
+    handtype, cardcount = classifyHand(es[0])
+    hands.append({
+      'hand': es[0]
+    , 'bid': int(es[1])
+    , 'info': (handtype, cardcount)
+    })
+  return hands
+
+def sortAllHands(allhands):
+  allFiveOfAKind = [x for x in allhands if x['info'][0] == 'Five of a kind']
+  allFourOfAKind = [x for x in allhands if x['info'][0] == 'Four of a kind']
+  allFullHouse = [x for x in allhands if x['info'][0] == 'Full house']
+  allThreeOfAKind = [x for x in allhands if x['info'][0] == 'Three of a kind']
+  allTwoPair = [x for x in allhands if x['info'][0] == 'Two pair']
+  allOnePair = [x for x in allhands if x['info'][0] == 'One pair']
+  allHighCard = [x for x in allhands if x['info'][0] == 'High card']
+  def cmp(a, b):
+    return  cmpHands(a['hand'], b['hand'])
+  allFiveOfAKind.sort(key=cmp_to_key(cmp))
+  allFourOfAKind.sort(key=cmp_to_key(cmp))
+  allFullHouse.sort(key=cmp_to_key(cmp))
+  allThreeOfAKind.sort(key=cmp_to_key(cmp))
+  allTwoPair.sort(key=cmp_to_key(cmp))
+  allOnePair.sort(key=cmp_to_key(cmp))
+  allHighCard.sort(key=cmp_to_key(cmp))
+  return allHighCard + allOnePair + allTwoPair + allThreeOfAKind \
+                     + allFullHouse + allFourOfAKind + allFiveOfAKind
+
+if __name__ == '__main__':
+  hands = sortAllHands(parse_input('day7/day7_input.txt'))
+  #hands = sortAllHands(parse_input('test_input.txt'))
+  print(*hands, sep='\n')
+  print(sum([i*x for i, x in enumerate([hand['bid'] for hand in hands], 1)]))
